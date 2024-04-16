3c586dfd2c7daaf7b3bffef3fef3518da5aa4524
Vinícius Castro <vinicastrolimavcl@gmail.com>
2024-04-15 23:03:23 -0300

    Feat: Novos campos

--- app/Http/Controllers/ApostasFeitasController.php
+++ app/Http/Controllers/ApostasFeitasController.php
@@ -13,83 +13,85 @@ use Illuminate\Support\Facades\Validator;
 class ApostasFeitasController extends Controller
 {
 
-    public function filter(Request $request){
+    public function filter(Request $request)
+    {
         try {
-
-             $roles = ['super_admin', 'socio', 'admin'];
-
-             if (in_array(Auth::user()->role, $roles)) {
-
+            $roles = ['super_admin', 'socio', 'admin'];
+    
+            if (in_array(Auth::user()->role, $roles)) {
                 $data = $request->all();
-
+    
                 $validator = Validator::make($data, [
                     'banca' => 'required|string',
                     'modalidade' => 'required|integer',
                     'inicio' => 'required|date',
                     'fim' => 'required|date',
                 ]);
-
+    
                 if ($validator->fails()) {
                     return response()->json(['errors' => $validator->errors()], 422);
                 }
-
+    
                 $banca = $request->input('banca');
                 $modalidade = $request->input('modalidade');
-                $inicio = $request->input('inicio')." 00:00";
-                $fim = $request->input('fim')." 23:59";
-
+                $inicio = $request->input('inicio') . " 00:00";
+                $fim = $request->input('fim') . " 23:59";
+    
                 $data = Apostas::whereBetween('created_at', [$inicio, $fim])
-                 ->where('tipo_jogo', $banca)
-                 ->where('jogo_id', $modalidade)
-                 ->get();
+                    ->where('tipo_jogo', $banca)
+                    ->where('jogo_id', $modalidade)
+                    ->distinct()
+                    ->get(['id', 'nome_usuario', 'usuario_id', 'tipo_jogo', 'jogo', 'valor_aposta', 'valor_premio', 'created_at', 'bilhete', 'concurso']);
+    
                 $dados = [];
-                $dados['info'] = [];
                 $valorTotal = 0;
                 $totalBilhetes = 0;
                 $totalUsuarios = [];
                 $concursos = [];
-
-                foreach($data as $info){
-
-                    $valorTotal += floatval(str_replace(',', '.', $info['valor_aposta']));
+    
+                foreach ($data as $info) {
+                    $valorTotal += floatval(str_replace(',', '.', $info->valor_aposta));
                     $totalBilhetes += 1;
-
-                    $date = new DateTime($info['created_at']);
-
-                    if(!in_array($info['usuario_id'], $totalUsuarios)){
-                        array_push($totalUsuarios, $info['usuario_id']);
+    
+                    if (!in_array($info->usuario_id, $totalUsuarios)) {
+                        $totalUsuarios[] = $info->usuario_id;
                     }
-
-                    if(!in_array($info['concurso'], $concursos)){
-                        array_push($concursos, $info['concurso']);
+    
+                    if (!in_array($info->concurso, $concursos)) {
+                        $concursos[] = $info->concurso;
                     }
-
-                    array_push($dados['info'], [
-                        'id' => $info['id'],
-                        'nome' => $info['nome_usuario'],
-                        'tipo_jogo' => $info['tipo_jogo'],
-                        'jogo' => $info['jogo'],
-                        'valor' => number_format($info['valor_aposta'], 2, ',', '.'),
-                        'premio' => number_format($info['valor_premio'], 2, ',', '.'),
+    
+                    $date = new DateTime($info->created_at);
+    
+                    $dados['info'][] = [
+                        'id' => $info->id,
+                        'nome' => $info->nome_usuario,
+                        'tipo_jogo' => $info->tipo_jogo,
+                        'jogo' => $info->jogo,
+                        'valor' => number_format($info->valor_aposta, 2, ',', '.'),
+                        'premio' => number_format($info->valor_premio, 2, ',', '.'),
                         'criacao' => $date->format('d/m/Y H:i:s'),
-                        'bilhete' => $info['bilhete'],
-                        'concurso' => $info['concurso'],
-                    ]);
+                        'bilhete' => $info->bilhete,
+                        'concurso' => $info->concurso,
+                        'usuario_id' => $info->usuario_id, // Adicionando usuario_id
+                    ];
                 }
-
+    
                 $dados['concursos'] = implode(', ', $concursos);
                 $dados['totalUsuarios'] = count($totalUsuarios);
                 $dados['totalBilhetes'] = $totalBilhetes;
-                $dados['valorTotal'] = number_format($valorTotal, 2, ',', '.') ;
-
+                $dados['valorTotal'] = number_format($valorTotal, 2, ',', '.');
+    
                 return response()->json(['success' => true, 'data' => $dados], 200);
             }
-
+    
             return response()->json(['success' => false], 403);
-        }catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             throw new Exception($th);
         }
     }
+    
+    
 
     public function store(Request $request)
     {
