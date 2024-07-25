8cbd726aab3a68a6385bce170b30fe93e71f37b0
Dener Gomes <52760360+alodener@users.noreply.github.com>
2024-07-25 13:34:52 -0300

    Update PartnerController.php

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -1311,15 +1311,38 @@ class PartnerController extends Controller
             if($client_data) {
                 $user_data = DB::connection($data_partner['connection'])->table('users')->where('email', $client_data->email)->first();
                 if($user_data) {
-                    // Atualiza o saldo disponível do usuário somando o total dos prêmios
-                    $new_value = $total_premio + floatval($user_data->available_withdraw);
-                    $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data->id)->update(['available_withdraw' => $new_value]);
+                    $old_value = floatval($user_data->available_withdraw);  // Recupera o valor antigo do saldo disponível
+                $new_value = $total_premio + $old_value;
+                $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data->id)->update(['available_withdraw' => $new_value]);
+                DB::connection($data_partner['connection'])->table('transact_balance')->insert([
+                    'user_id_sender' => $user_data->id,
+                    'user_id' => $user_data->id,
+                    'value' => $total_premio,
+                    'old_value' => $old_value,
+                    'value_a' => $new_value,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                    'type' => 'Premiacao JB id: ' .  $game_id,
+                    'wallet' => 'Premiacao'
+                ]);
                 } else {
                     // Atualiza o usuário padrão se o usuário específico não for encontrado
                     $user_data_default = DB::connection($data_partner['connection'])->table('users')->where('email', 'mercadopago@mercadopago.com')->first();
                     if($user_data_default) {
-                        // Atualiza o saldo disponível do usuário padrão somando o total dos prêmios
-                        $new_value = $total_premio + floatval($user_data_default->available_withdraw);
+                        $old_value = floatval($user_data_default->available_withdraw);  // Recupera o valor antigo do saldo disponível
+                    $new_value = $total_premio + $old_value;
+                    $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data_default->id)->update(['available_withdraw' => $new_value]);
+                    DB::connection($data_partner['connection'])->table('transact_balance')->insert([
+                        'user_id_sender' => $user_data_default->id,
+                        'user_id' => $user_data_default->id,
+                        'value' => $total_premio,
+                        'old_value' => $old_value,
+                        'value_a' => $new_value,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                        'type' => 'Premiacao JB id: ' .  $game_id,
+                        'wallet' => 'Premiacao'
+                    ]);
                     }
                 }
             }
@@ -1355,14 +1378,38 @@ class PartnerController extends Controller
             if($client_data) {
                 $user_data = DB::connection($data_partner['connection'])->table('users')->where('email', $client_data->email)->first();
                 if($user_data) {
-                    $new_value = $total_premio + floatval($user_data->available_withdraw);
-                    $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data->id)->update(['available_withdraw' => $new_value]);
+                    $old_value = floatval($user_data->available_withdraw);  // Recupera o valor antigo do saldo disponível
+                        $new_value = $total_premio + $old_value;
+                        $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data->id)->update(['available_withdraw' => $new_value]);
+                            DB::connection($data_partner['connection'])->table('transact_balance')->insert([
+                                'user_id_sender' => $user_data->id,
+                                'user_id' => $user_data->id,
+                                'value' => $total_premio,
+                                'old_value' => $old_value,
+                                'value_a' => $new_value,
+                                'created_at' => now(),
+                                'updated_at' => now(),
+                                'type' => 'Premiacao de jogo loteria id: ' .  $game_id,
+                                'wallet' => 'Premiacao'
+                            ]);
                 } else {
                     // Atualiza o usuário padrão se o usuário específico não for encontrado
                     $user_data_default = DB::connection($data_partner['connection'])->table('users')->where('email', 'mercadopago@mercadopago.com')->first();
                     if($user_data_default) {
-                        $new_value = $total_premio + floatval($user_data_default->available_withdraw);
+                        $old_value = floatval($user_data_default->available_withdraw);  // Recupera o valor antigo do saldo disponível
+                        $new_value = $total_premio + $old_value;
                         $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data_default->id)->update(['available_withdraw' => $new_value]);
+                            DB::connection($data_partner['connection'])->table('transact_balance')->insert([
+                            'user_id_sender' => $user_data_default->id,
+                            'user_id' => $user_data_default->id,
+                            'value' => $total_premio,
+                            'old_value' => $old_value,
+                            'value_a' => $new_value,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                            'type' => 'Premiacao de jogo loteria id: ' .  $game_id,
+                            'wallet' => 'Premiacao'
+                            ]);
                     }
                 }
             }
