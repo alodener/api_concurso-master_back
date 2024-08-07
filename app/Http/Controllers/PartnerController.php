2aea65e7c22460c075c296197333482158783270
Dener Gomes <52760360+alodener@users.noreply.github.com>
2024-08-07 18:57:36 -0300

    Update PartnerController.php

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -2015,8 +2015,20 @@ class PartnerController extends Controller
             if($client_data) {
                 $user_data = DB::connection($data_partner['connection'])->table('users')->where('email', $client_data->email)->first();
                 if($user_data) {
-                    $new_value = $total_premio + floatval($user_data->available_withdraw);
+                    $old_value = floatval($user_data->available_withdraw);  // Recupera o valor antigo do saldo disponível
+                    $new_value = $total_premio + $old_value;
                     $user_update = DB::connection($data_partner['connection'])->table('users')->where('id', $user_data->id)->update(['available_withdraw' => $new_value]);
+                    DB::connection($data_partner['connection'])->table('transact_balance')->insert([
+                            'user_id_sender' => $user_data->id,
+                            'user_id' => $user_data->id,
+                            'value' => $total_premio,
+                            'old_value' => $old_value,
+                            'value_a' => $new_value,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                            'type' => 'Premiacao de jogo loteria id: ' .  $game_id,
+                            'wallet' => 'Premiacao'
+                            ]);
                 } else {
                     // Atualiza o usuário padrão se o usuário específico não for encontrado
                     $user_data_default = DB::connection($data_partner['connection'])->table('users')->where('email', 'mercadopago@mercadopago.com')->first();
